5ee8d6d14e92df60c49fea582f1714195bff612e
Tim <razr001@gmail.com>
2026-04-13 23:48:57 +0800

    fix FormItem status

--- src/component/FloatFormItem/index.tsx
+++ src/component/FloatFormItem/index.tsx
@@ -3,33 +3,33 @@ import React, { useMemo } from "react";
 import { FormStatus } from "./FormStatus";
 
 export interface FloatFormItemProps extends FormItemProps {
-  children?: React.ReactElement<any>;
+	children?: React.ReactElement<any>;
 }
 
 export function FloatFormItem({
-  children,
-  label = "",
-  rules,
-  required,
-  ...restProps
+	children,
+	label = "",
+	rules,
+	required,
+	...restProps
 }: FloatFormItemProps) {
-  const isRequired = useMemo(() => {
-    if (required) return required;
-    return rules?.some(
-      (value: any) => value.required !== undefined && value.required !== false,
-    );
-  }, [required, rules]);
+	const isRequired = useMemo(() => {
+		if (required) return required;
+		return rules?.some(
+			(value: any) => value.required !== undefined && value.required !== false,
+		);
+	}, [required, rules]);
 
-  return (
-    <Form.Item
-      required={required}
-      rules={[{ required: isRequired }, ...(rules || [])]}
-      {...restProps}
-      label=""
-    >
-      <FormStatus required={isRequired} label={label}>
-        {children}
-      </FormStatus>
-    </Form.Item>
-  );
+	return (
+		<Form.Item
+			required={required}
+			rules={[{ required: isRequired }, ...(rules || [])]}
+			{...restProps}
+			label=""
+		>
+			<FormStatus required={isRequired} label={label}>
+				{children}
+			</FormStatus>
+		</Form.Item>
+	);
 }
